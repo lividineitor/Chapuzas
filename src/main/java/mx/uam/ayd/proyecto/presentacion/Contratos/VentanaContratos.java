4e9d8b99a4911cf54a28569ee40c4abcbe224c1f
CesarGuzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2022-01-24 22:13:42 -0600

    otro

--- src/main/java/mx/uam/ayd/proyecto/presentacion/Contratos/VentanaContratos.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/Contratos/VentanaContratos.java
@@ -1,22 +1,34 @@
 package mx.uam.ayd.proyecto.presentacion.Contratos;
 import mx.uam.ayd.proyecto.presentacion.Contratos.ControlContratos;
 import javax.swing.JFrame;
-import org.springframework.stereotype.Component;
+import javax.swing.JPanel;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JButton;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 
 //Iturbe Pineda
 
-@SuppressWarnings("serial")
 @Component
 public class VentanaContratos extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public VentanaContratos() {
-		
-		
-		
+			
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
@@ -37,4 +49,27 @@ public class VentanaContratos extends JFrame {
 	public void termina() {
 		this.setVisible(false);		
 	}
+    private static JPanel GetPdfPanel(String NombreArch) {
+        PDDocument doc = null;
+        try {
+            doc = PDDocument.load(new File(NombreArch));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final PDFRenderer renderer = new PDFRenderer(doc);
+        JPanel panel = new JPanel() {
+        
+			private static final long serialVersionUID = 1L;
+
+			@Override
+            protected void paintComponent(Graphics g) {
+                try {
+                    renderer.renderPageToGraphics(0, (Graphics2D) g, 0.5f);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        return panel;
+    }
 }

558aa17ca02662f240af34d5b39b2f8c55930706
Monserrat Iturbe <monse.iturbe31@gmail.com>
2022-01-24 18:17:02 -0600

    comentario

---
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/Contratos/ControlContratos.java
@@ -0,0 +1,5 @@
+package mx.uam.ayd.proyecto.presentacion.Contratos;
+
+public class ControlContratos {
+
+}
---
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/Contratos/VentanaContratos.java
@@ -0,0 +1,40 @@
+package mx.uam.ayd.proyecto.presentacion.Contratos;
+import mx.uam.ayd.proyecto.presentacion.Contratos.ControlContratos;
+import javax.swing.JFrame;
+import org.springframework.stereotype.Component;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+//Iturbe Pineda
+
+@SuppressWarnings("serial")
+@Component
+public class VentanaContratos extends JFrame {
+	
+	public VentanaContratos() {
+		
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(335, 227, 89, 23);
+		getContentPane().add(btnCancelar);
+		
+		//Listeners
+		
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				termina();
+			}
+			
+		});
+	}
+	public void termina() {
+		this.setVisible(false);		
+	}
+}
